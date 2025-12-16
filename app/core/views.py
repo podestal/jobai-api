6149a732d3d97e841e9f46dcf5c90fc17542bec5
podestal <luisrodriguezpodesta@gmail.com>
2025-12-16 07:20:41 -0500

    updates

--- app/core/views.py
+++ app/core/views.py
@@ -1,17 +1,3 @@
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import get_user_model
+from django.shortcuts import render
 
-User = get_user_model()
-
-
-def get_tokens_for_user(user):
-    """Generate JWT tokens for a user"""
-    refresh = RefreshToken.for_user(user)
-    return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
-    }
+# Create your views here.
